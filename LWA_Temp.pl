7a58066d68e979c9b89a83fe4f131ac526e180ac
mike1098 <6850690+mike1098@users.noreply.github.com>
2019-11-17 16:58:33 +0100

    Added correct handling of OS signals
    Added the correct handling of OS signals to gracefully shutdown DB connection in case of CTR-C or shutdown.

--- LWA_Temp.pl
+++ LWA_Temp.pl
@@ -60,11 +60,11 @@ my $InBytes;my $OutBytes;my $ErrorFlags;my $count_out;my $BlockingFlags;
 while ($timeout>0)
   {
   ###################################### Write initial request ###################################################
-  print "Write initial request\n" if $debug;
+  print "Write initial request\n" if ($debug == 2);
   $count_out = $port->write($initialReq);    
   warn " write failed\n" unless ($count_out);
   warn " write incomplete sent:",$count_out,"\n" if ( $count_out != length($initialReq) );
-	if ($debug) {
+	if ($debug == 2) {
 	  ($BlockingFlags, $InBytes, $OutBytes, $ErrorFlags) = $port->status;
 	  print " Wrote: ", $count_out, " Characters ", $OutBytes, " Bytes ", "Error: ", $ErrorFlags, " WriteDrain: ",$port->write_drain,"\n";
 	  print " wrote: ", unpack('H*',$initialReq), "\n";
@@ -72,16 +72,16 @@ while ($timeout>0)
 	};
 	###################################### Read from port #########################################################
 	my ($count,$saw)=$port->read(12); # will read _up to 12 chars
-	if ($debug) {
+	if ($debug == 2) {
 	  ($BlockingFlags, $InBytes, $OutBytes, $ErrorFlags) = $port->status;
 	  print " Read: ", $count, " Characters ", $InBytes, " Bytes ", "Error: ", $ErrorFlags, " WriteDrain: ",$port->write_drain,"\n";
 	  print " Got: ", unpack('H*',$saw), "\n";
 	};
 	##################################### If we get the right answer read the second reply #########################################
 	if (unpack('H*',$saw) eq unpack('H*',$initialReq)) {
-		print "Read second 12 char\n" if $debug;		
+		print "Read second 12 char\n" if ($debug == 2);		
 		my ($count,$saw)=$port->read(12); # will read _up to_ 24 chars
-		if ($debug) {
+		if ($debug == 2) {
 		  ($BlockingFlags, $InBytes, $OutBytes, $ErrorFlags) = $port->status;
 		  print " Read: ", $count, " Characters ", $InBytes, " Bytes ", "Error: ", $ErrorFlags, " WriteDrain: ",$port->write_drain,"\n";
 		  print " Got: ", unpack('H*',$saw), "\n";
@@ -91,47 +91,52 @@ while ($timeout>0)
 			while ($timeout>0) {
 			  $port->purge_all;
 			  ################################################ Get Sensors from DB ######################################
-			  my $res = $dbh->selectall_arrayref("SELECT ID, Address, Name FROM sensors;");
+			  my $res = $dbh->selectall_arrayref("SELECT ID, Address, Name FROM sensors where Type='LWZ';");
 			  ################################################ Request Values from LWZ ##################################				
 			  foreach my $row (@$res) {
+			    $SIG{'INT'} = 'IGNORE'; # Prevent from CTRL-C to allow a complete fetch of all sensors
   			    my ($id, $address) = @$row;
 				#check of address has valid values		
     			    my $Req = pack('H*',${$row}[1]);	
 				  $count_out = $port->write($Req);
 				  warn " write failed\n" unless ($count_out);
 				  warn " write incomplete sent:",$count_out,"\n" if ( $count_out != length($Req) );
-					if ($debug) {
+					if ($debug == 2) {
 					  ($BlockingFlags, $InBytes, $OutBytes, $ErrorFlags) = $port->status;
 					  print " Wrote: ", $count_out, " Characters ", $OutBytes, " Bytes ", "Error: ", $ErrorFlags, " WriteDrain: ",$port->write_drain,"\n";
 					  print " Read first 12 char\n" if $debug;
 					  };
 					my ($count,$saw)=$port->read(12); # will read _up to 12 chars
-					if ($debug) {
+					if ($debug == 2) {
 					  ($BlockingFlags, $InBytes, $OutBytes, $ErrorFlags) = $port->status;
 					  print " Read: ", $count, " Characters ", $InBytes, " Bytes ", "Error: ", $ErrorFlags, " WriteDrain: ",$port->write_drain,"\n";
 					};
 				  if (unpack('H*',$saw) eq unpack('H*',$Req)) {
-				     print "Read second 12 char and convert to values\n" if $debug;
+				     print "Read second 12 char and convert to values\n" if ($debug == 2);
 				     my ($count,$saw)=$port->read(12); # will read _up to 12 chars
-				     if ($debug) {
+				     if ($debug == 2) {
 					  ($BlockingFlags, $InBytes, $OutBytes, $ErrorFlags) = $port->status;
 					  print " Read: ", $count, " Characters ", $InBytes, " Bytes ", "Error: ", $ErrorFlags, " WriteDrain: ",$port->write_drain,"\n";
 					};
 				     eval {
 				     my ($first,$second) = unpack ('x6s>s>',$saw);# || warn print "could not unpack response\n";
-				     
-				     print "First Value: $first, second value: $second\n" if $debug;
+				     print "------------------------------------------------------------------------------\n" if $debug;
+				     print "First Value: $first, second value: $second\n" if ($debug == 1);
 				     my $value;
-				     if ($first > 0) {$value = $first} elsif ($second > 0) {$value = $second} else {$value=0};
-				     print "Insert $value for sensor ${$row}[2]\n" if $debug;
-				     $dbh->do("INSERT INTO 'S_Values' (Sensor_ID, S_Value) VALUES (${$row}[0],$value);") or warn $DBI::errstr;
+				     if ($first == 0) {$value = $second} elsif ($second == 0) {$value = $first} else {$value=0};
+				     
+				     print "Insert $value for sensor ${$row}[2] with id ${$row}[0]\n" if $debug;
+				     $dbh->do("INSERT INTO 'S_Values' (Sensor_ID, S_Value) VALUES (${$row}[0],$value);");# or warn $DBI::errstr;
 				     };				     
-				     $dbh->commit();
+				     #$dbh->commit();
+				     
 				  } else {
 					print "Got not what I expected\n";
 				  };
 				
 				};
+				$SIG{'INT'} = 'INT_handler'; #Allow CTRL-C and run function INT_handler to shutdown DB connection
+				print "===============================================================================\n" if $debug;
 				sleep $STALL_DEFAULT;
 				###########################################################################################################
 
@@ -139,7 +144,9 @@ while ($timeout>0)
 		}
 		else {
 		     sleep $STALL_DEFAULT;
+		     $SIG{'INT'} = 'INT_handler'; #Allow CTRL-C and run function INT_handler to shutdown DB connection
 		     $timeout--;
+		     print "Not connected to LWA. Timeout in $timeout seconds\n";
 		}	
 	}
 print "$timeout No response from LWZ  \n";
@@ -151,3 +158,10 @@ $port->close || warn "close failed";
 print "Exit because of no connection to LWZ";
 undef $port;
 $dbh->disconnect();
+
+# This function handles a CTRL-C to gracefully shutdown DB connection
+sub INT_handler {
+  print "CTRL-C Pressed; Closing DB connection\n";
+  $dbh->disconnect();
+  exit(0);
+}
